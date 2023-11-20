8b2ecf25437af012c07ef8c86a4020e00840f762
harupy <harupy@users.noreply.github.com>
2023-11-20 03:10:42 +0000

    deploy: 7462e9bb6aaae2c73ce4e9972aa89ebd879dcd85

---
+++ assets/js/78c39f1a.7d3be37d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[936],{3639:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"MLflow 2.8.1","permalink":"/mlflow-website-docusaurus/releases/v2.8.1","unlisted":false},{"title":"MLflow 2.8.0","permalink":"/mlflow-website-docusaurus/releases/v2.8.0","unlisted":false}]}')}}]);
